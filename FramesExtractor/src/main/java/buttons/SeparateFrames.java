6f1a8620a725ce058660e829a41c0f39c0b20522
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-27 20:32:08 -0300

    Remake of the Java Frame Extractor

--- FramesExtractor/src/main/java/buttons/SeparateFrames.java
+++ FramesExtractor/src/main/java/buttons/SeparateFrames.java
@@ -1,21 +1,21 @@
 package buttons;
 
-import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.copiedstuff.FrameGrabber;
 import progressbars.ProgressBar;
 import videorelated.ExtractFrames;
 import videorelated.FrameUI;
-import videorelated.ThreadExtractFrames;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import static labels.SelectFolder.folderPath;
 import static labels.SelectVideo.videoPath;
 
 public class SeparateFrames extends JButton implements ActionListener {
 
-    public SeparateFrames(JFrame mainFrame){
+    public SeparateFrames(JFrame mainFrame) {
         this.setText("Separate frames");
         this.addActionListener(this);
 
@@ -24,24 +24,26 @@ public class SeparateFrames extends JButton implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(videoPath == null || folderPath == null){
+        if (videoPath == null || folderPath == null) {
             JOptionPane.showMessageDialog(null, "You need to select both video and folder to save", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        else if(videoPath.getAbsolutePath().isEmpty() || folderPath.getAbsolutePath().isEmpty()){
+        else if (videoPath.getAbsolutePath().isEmpty() || folderPath.getAbsolutePath().isEmpty()) {
             JOptionPane.showMessageDialog(null, "You need to select both video and folder to save", "Error", JOptionPane.ERROR_MESSAGE);
         }
-        else{
+        else {
             try {
                 FrameUI.progress.setTotalAmountOfFrames(ExtractFrames.getAmountOfFrames());
-                System.out.println("Successfully set the total amount of frames: "+ ProgressBar.totalAmountOfFrames);
-
-                ThreadExtractFrames extract = new ThreadExtractFrames();
-                extract.run();
-            } catch (FFmpegFrameGrabber.Exception ex) {
+            } catch (FrameGrabber.Exception ex) {
                 ex.printStackTrace();
             }
+            System.out.println("Successfully set the total amount of frames: " + ProgressBar.totalAmountOfFrames);
 
-
+            try {
+                ExtractFrames.separateFrames();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
         }
     }
+
 }
