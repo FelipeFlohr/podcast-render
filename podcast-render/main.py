98cde7e434ae61f45935b94dec01ca7a5d825260
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-27 22:22:48 -0300

    The program now renders the video almost perfectly

--- podcast-render/main.py
+++ podcast-render/main.py
@@ -20,14 +20,21 @@ class ImageAnalyzer:
             video.save_frame(f"{self.frames_folder}/{i}.png")
             print("Iteration #{} done.".format(i))
 
-    def generate_casters_timestamps(self, felipe_pos, gabriel_pos):
+    def get_total_amount_of_frames(self):
         total_files = 0
 
         # Counting how many files there is on the folder
         for i in os.listdir(self.frames_folder):
             total_files += 1
 
-        print(f'total files {total_files}')
+        return total_files
+
+    def generate_casters_timestamps(self, felipe_pos, gabriel_pos):
+        total_files = 0
+
+        # Counting how many files there is on the folder
+        for i in os.listdir(self.frames_folder):
+            total_files += 1
 
         # Casters' list order: Felipe, Gabriel
 
@@ -41,7 +48,7 @@ class ImageAnalyzer:
             image = Image.open(f"{self.frames_folder}\\{i}.png")
             pixel = image.load()
 
-            if pixel[felipe_pos[0], felipe_pos[1]][0] > 30:
+            if 30 < pixel[felipe_pos[0], felipe_pos[1]][0] < 67:
                 pixeldict = {"speaking": True}
                 felipe.append(pixeldict.copy())
             else:
@@ -49,7 +56,7 @@ class ImageAnalyzer:
                 felipe.append(pixeldict.copy())
 
             # This will create a dictionary for Gabriel
-            if pixel[gabriel_pos[0], gabriel_pos[1]][0] > 30:
+            if 30 < pixel[gabriel_pos[0], gabriel_pos[1]][0] < 67:
                 pixeldict = {"speaking": True}
                 gabriel.append(pixeldict.copy())
             else:
@@ -62,7 +69,42 @@ class ImageAnalyzer:
         return casters
 
 
+class MovieRendering:
+    def __init__(self, video_path: str, video_output: str, timestamps, qnt_frames: int):
+        self.video_path = video_path
+        self.video_output = video_output
+        self.timestamps = timestamps
+        self.amount_of_frames = qnt_frames
+
+    def generate_frames(self):
+        frames = list()
+
+        for i in range(self.amount_of_frames):
+            if timestamps[0][i]["speaking"] and timestamps[1][i]["speaking"]:
+                frames.append("podcast-render/files/felipegabriel.png")
+            elif timestamps[0][i]["speaking"]:
+                frames.append("podcast-render/files/felipe.png")
+            elif timestamps[1][i]["speaking"]:
+                frames.append("podcast-render/files/gabriel.png")
+            else:
+                frames.append("podcast-render/files/blank.png")
+
+        return frames
+
+    def render_movie(self, frames_stamps):
+        video_audio = VideoFileClip(self.video_path)
+        video_audio.audio.write_audiofile("temp.mp3")
+
+        clip = ImageSequenceClip(frames_stamps, fps=60)
+        clip.set_audio("temp.mp3")
+        clip.write_videofile("output.mp4")
+
+
 if __name__ == "__main__":
     image_analysis = ImageAnalyzer("C:\\Users\\Felipe\\Desktop\\Frames", "video.mp4")
-    # (252, 742), (252, 801)
     timestamps = image_analysis.generate_casters_timestamps((252, 742), (252, 801))
+    qnt_frames = image_analysis.get_total_amount_of_frames()
+
+    movie_render = MovieRendering("video.mp4", "fodeta/saida.mp4", timestamps, qnt_frames)
+    frames = movie_render.generate_frames()
+    movie_render.render_movie(frames)
